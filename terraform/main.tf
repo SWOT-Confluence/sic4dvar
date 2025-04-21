c7656339df8c23b9b9783ff7d7c8b21a295f6d05
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-21 16:08:54 -0400

    Initial deployment infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    encrypt = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = local.default_tags
+  }
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id = sensitive(data.aws_caller_identity.current.account_id)
+  default_tags = length(var.default_tags) == 0 ? {
+    application : var.app_name,
+    environment : lower(var.environment),
+    version : var.app_version
+  } : var.default_tags
+}
+
+module "confluence-sic4dvar" {
+  source      = "./modules/sic4dvar"
+  app_name    = var.app_name
+  app_version = var.app_version
+  aws_region  = var.aws_region
+  environment = var.environment
+  prefix      = var.prefix
+}
